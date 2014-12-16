dd49de52d5f3979edef6c8cae779850414eb8833
vit <shiegin@gmail.com>
2014-12-16 08:17:36 +0300

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,11 @@
 user = User.new
 #user.email = 'test@example.com'
 #user.encrypted_password = '#$taawktljasktlw4aaglj'
-user.email = 'qqq@qqq.com'
+#user.email = 'qqq@qqq.com'
+user.email = 'shiegin@gmail.com'
 #user.encrypted_password = 'qqqqqqqq'
 user.password = 'qqqqqqqq'
+user.is_admin = true
 user.save!
 
 
