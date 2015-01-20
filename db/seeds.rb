78d839c627f4ea85866d5ace017feb842e65215b
vit <shiegin@gmail.com>
2015-01-20 04:22:43 +0300

    fixed reviews for revisions on paper page, r and ce offices

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,9 @@
 
-#User.create(email: 'test@example.com', encrypted_password: '#$taawktljasktlw4aaglj')
 
 user = User.new
-#user.email = 'test@example.com'
-#user.encrypted_password = '#$taawktljasktlw4aaglj'
-#user.email = 'qqq@qqq.com'
+user.first_name = 'Vitaliy'
+user.last_name = 'Shiegin'
 user.email = 'shiegin@gmail.com'
-#user.encrypted_password = 'qqqqqqqq'
 user.password = 'qqqqqqqq'
 user.is_admin = true
 user.save!
